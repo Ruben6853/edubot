30a0a184cd769900a45b61ac02d416aefcc86539
ruben <r.j.vanduuren@student.tudelft.nl>
2026-02-27 15:15:15 +0100

    revert some refactors that went wrong in the example trajs

--- ros_ws/src/controllers/include/controllers/example_pos_traj.hpp
+++ ros_ws/src/controllers/include/controllers/example_pos_traj.hpp
@@ -1,19 +1,19 @@
 #include "rclcpp/rclcpp.hpp"
 #include "trajectory_msgs/msg/joint_trajectory.hpp"
 
-class Controller : public rclcpp::Node
+class ExampleTraj : public rclcpp::Node
 {
 private:
-    rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr _pos_cmd_publisher;
+    rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr _publisher;
     rclcpp::TimerBase::SharedPtr _timer;
 
     rclcpp::Time _beginning;
     void _timer_callback();
 
-    std::vector<double> home_joint_pos;
+    std::vector<double> home;
 
 public:
-    Controller();
+    ExampleTraj();
 };
 
     
